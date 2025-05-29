f699d3a040907d7ee4a54e1db3341630935e163e
Uy Binh <uybinh@gmail.com>
2025-05-29 17:42:17 +0700

    Add color-coded warning for fzf disablement in home directory

--- .zsh/plugins/fzf-wrapper.plugin.zsh
+++ .zsh/plugins/fzf-wrapper.plugin.zsh
@@ -1,9 +1,13 @@
 fzf_conditional() {
+  local RED='\033[0;31m'
+  local RESET='\033[0m'
+
   if [[ "$PWD" == "$HOME" ]]; then
-    echo "fzf disabled in home directory"
+    echo -e "${RED}fzf disabled in home directory${RESET}"
     return 1
   else
     command fzf "$@"
   fi
 }
+
 alias fzf='fzf_conditional'
